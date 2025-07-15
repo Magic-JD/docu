d444ac468226799fcfbcf63a2554cff74e4721d7
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2025-07-15 19:04:23 +0800

    updating help documentation

--- src/cli/command.rs
+++ src/cli/command.rs
@@ -3,7 +3,7 @@ use clap::{Parser, Subcommand};
 #[derive(Debug, Parser)]
 #[command(
     name = "docu",
-    about = "Easily document your CLI commands",
+    about = "A CLI tool to document and manage your favorite command-line scriptlets. A scriptlet is a small piece of code or a command that you use frequently. This command allows you to save and document them for future reference.",
     version = env!("CARGO_PKG_VERSION"),
     author = "Joseph Daunt",
     after_help = "For more details, visit https://github.com/Magic-JD/docu"
@@ -15,34 +15,35 @@ pub struct Cli {
 
 #[derive(Debug, Subcommand)]
 pub enum Commands {
-    /// Document a new scriptlet
+    /// Add a new scriptlet to your documentation.
     Add {
-        /// The full invocation of your tool + scriptlet
-        ///
-        /// Example:
-        /// docu add "sed -i '1{s/ /_/g}'" products.csv
+        /// The full command invocation to be documented.
+        /// It should be wrapped in quotes, and any internal quotation marks or $ should be
+        /// escaped.
         #[arg(required = true)]
         args: String,
     },
 
-    /// Show existing documentation
+    /// Display saved scriptlets. You can either display all scriptlets or filter them by a specific tool.
     Show {
-        /// If omitted, lists all tools; otherwise shows only this tool's scriptlets
+        /// The specific tool to show scriptlets for. If omitted, all scriptlets for all tools are shown.
         tool: Option<String>,
     },
 
-    /// Search existing documentation
+    /// Search for scriptlets by keywords.
     Search {
-        /// Searches through the documentation for matching tools
+        /// The keyword(s) to search for in the scriptlet documentation.
         search: Vec<String>,
     },
 
-    // Remove a scriptlet by id
+    /// Remove one or more scriptlets by their ID. You can get the ID of
+    /// a scriptlet by using the `show` or `search` command.
     Remove {
-        // Removes the given ids
+        /// The ID(s) of the scriptlet(s) to remove.
         ids: Vec<i64>,
     },
 
-    /// Generate the default config file
+    /// Generate a default configuration file. This command creates a `docu.toml` file in the application's
+    /// default configuration directory, pre-populated with default settings.
     GenerateConfig,
 }
